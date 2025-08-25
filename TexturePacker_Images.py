3b8a5d1059bc48df903e100ed37e614bf995b424
Mark Andrew Azzopardi <mark25gundam@gmail.com>
2025-08-25 22:14:30 +0200

    texture examples upload + load all textures + sort textures

---
+++ TexturePacker_Images.py
@@ -0,0 +1,40 @@
+﻿import tkinter
+from tkinter import filedialog
+from PIL import Image
+
+input_images = filedialog.askopenfilenames(title = "Select Image Files")
+
+basecolor_str = "basecolor"
+roughness_str = "roughness"
+height_str = "height"
+ambientocclusion_str = "ambientOcclusion"
+normal_str = "normal"
+
+for basecolorpath in input_images:
+    if basecolor_str in basecolorpath:
+        basecolor = basecolorpath
+
+for roughnesspath in input_images:
+    if roughness_str in roughnesspath:
+        roughness = roughnesspath
+
+for heightpath in input_images:
+    if height_str in heightpath:
+        height = heightpath
+
+for ambientocclusionpath in input_images:
+    if ambientocclusion_str in ambientocclusionpath:
+        ambientocclusion = ambientocclusionpath
+
+for normalpath in input_images:
+    if normal_str in normalpath:
+        normal = normalpath
+
+print("Basecolor path is: " + basecolor)
+print("Roughness path is: " + roughness)
+print("Height path is: " + height)
+print("Ambientocclusion path is: " + ambientocclusion)
+print("Normal path is: " + normal)
+
+
+
