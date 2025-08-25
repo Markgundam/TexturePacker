00bbcc38d54087b82e69ad1e30762befb7c8e7d6
Mark Andrew Azzopardi <mark25gundam@gmail.com>
2025-08-25 22:13:49 +0200

    load image, get channels through user input, save separate and merged images

--- TexturePacker_Data.py
+++ TexturePacker_Data.py
@@ -1,5 +1,24 @@
-﻿from PIL import Image
+﻿from tkinter import filedialog
+from PIL import Image
 
-image = Image.open("Picture.png")
+file_path = filedialog.askopenfilename()
+channel_1_input = input("Enter channel 1: ")
+channel_2_input = input("Enter channel 2: ")
+channel_3_input = input("Enter channel 3: ")
 
-image.show()
+# load image
+image = Image.open(file_path)
+
+# get channels from image and save them in separate vars
+img_channel_1 = image.getchannel(channel_1_input)
+img_channel_2 = image.getchannel(channel_2_input)
+img_channel_3 = image.getchannel(channel_3_input)
+
+# save separate channels as images
+img_channel_1.save("R.png")
+img_channel_2.save("G.png")
+img_channel_3.save("B.png")
+
+# save swizzled image
+image_merged = Image.merge("RGB", (img_channel_1, img_channel_2, img_channel_3))
+image_merged.save(channel_1_input + channel_2_input + channel_3_input + ".png")
