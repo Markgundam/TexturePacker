ecfe3c751d496c22d2de4362508d49ae91e474ae
Mark Andrew Azzopardi <mark25gundam@gmail.com>
2025-08-27 10:21:06 +0200

    Input Options Dictionary for easier selection with keys

--- TexturePacker_Images.py
+++ TexturePacker_Images.py
@@ -1,36 +1,75 @@
-﻿from logging import exception
+﻿from enum import Enum
+from logging import exception
+from random import choice
 from tkinter import filedialog
 from PIL import Image
 
 input_images = filedialog.askopenfilenames(title="Select Image Files")
+input_options = {}
 
 for path in input_images:
     if "basecolor" in path:
         color = Image.open(path)
-        color.save("BaseColor.png")
+        input_options["basecolor"] = 1
         continue
 
     if "normal" in path:
         normal = Image.open(path)
-        normal.save("Normal.png")
+        input_options["normal"] = 2
         continue
 
     if "roughness" in path:
         roughness = Image.open(path)
-        roughness.save("Roughness.png")
+        input_options["roughness"] = 3
         continue
 
     if "height" in path:
         height = Image.open(path)
-        height.save("Height.png")
+        input_options["height"] = 4
         continue
 
     if "ambientOcclusion" in path:
         ambient_occlusion = Image.open(path)
-        ambient_occlusion.save("AmbientOcclusion.png")
+        input_options["ambient_occlusion"] = 5
         continue
 
     raise Exception
 
-image_merged = Image.merge("RGBA", (normal.getchannel("R"), normal.getchannel("G"), ambient_occlusion.getchannel("R"), height.getchannel("R")))
-image_merged.save("RGBA.png")
+def input_texture():
+
+    for key, options in input_options.items():
+        print(options, key)
+
+    input_1 = int(input("Select input image: "))
+
+    if input_1 == 1:
+        print("Color selected")
+        input_1 = color
+
+    elif input_1 == 2:
+        print("Normal selected")
+        input_1 = normal
+
+    elif input_1 == 3:
+        print("Roughness selected")
+        input_1 = roughness
+
+    elif input_1 == 4:
+        print("Height selected")
+        input_1 = height
+
+    elif input_1 == 5:
+        print("AO selected")
+        input_1 = ambient_occlusion
+
+    elif input_1 != [1, 2, 3, 4, 5]:
+        print("Invalid input")
+
+number_of_inputs = int(input("Number of input images: "))
+
+for inputs in range(number_of_inputs):
+            input_texture()
+
+
+#image_merged = Image.merge("RGBA", (normal.getchannel("R"), normal.getchannel("G"), ambient_occlusion.getchannel("R"), height.getchannel("R")))
+#image_merged.save("RGBA.png")
